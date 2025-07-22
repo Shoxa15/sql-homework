1f8d4e266e2e38ff693af52a7110811f3f733b43
Shoxa15 <odilovshoxrux777@gmail.com>
2025-07-22 12:34:21 +0500

    Update hw2.sql

--- lesson-2/homework/hw2.sql
+++ lesson-2/homework/hw2.sql
@@ -28,3 +28,90 @@ CREATE TABLE Departments (
     DepartmentName VARCHAR(50)
 );
 DELETE FROM Employees;
+--------------------------------------------------------
+--11-task:
+CREATE TABLE #TempDepartments (
+    DepartmentName VARCHAR(50)
+);
+INSERT INTO #TempDepartments (DepartmentName)
+VALUES 
+('Финансы'),
+('Маркетинг'),
+('Кадры'),
+('Продажи'),
+('ИТ');
+INSERT INTO Departments (DepartmentName)
+SELECT DepartmentName FROM #TempDepartments;
+DROP TABLE #TempDepartments;
+--------------------------------------------------------
+--12-task:
+UPDATE Employees
+SET Department = 'Руководство'
+WHERE Salary > 5000;
+--------------------------------------------------------
+--13-task:
+DELETE FROM Employees;
+--------------------------------------------------------
+--14-task:
+ALTER TABLE Employees
+DROP COLUMN Department;
+--------------------------------------------------------
+--15-task:
+EXEC sp_rename 'Employees', 'StaffMembers';
+--------------------------------------------------------
+--16-task:
+DROP TABLE Departments;
+--------------------------------------------------------
+------------------------------------------------------------
+--task-17:
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2) CHECK (Price > 0),
+    Description VARCHAR(255) 
+    );
+------------------------------------------------------------
+--task-18: 
+ALTER TABLE Products
+ADD StockQuantity INT DEFAULT 50;
+------------------------------------------------------------
+--task-19:
+EXEC sp_rename 'Products.Category', 'ProductCategory', 'COLUMN';
+------------------------------------------------------------
+--task-20:
+INSERT INTO Products (ProductID, ProductName, ProductCategory, Price, Description)
+VALUES 
+(1, 'Ноутбук', 'Электроника', 1500.00, 'Мощный игровой ноутбук'),
+(2, 'Смартфон', 'Электроника', 800.00, 'Флагманский смартфон'),
+(3, 'Стул', 'Мебель', 120.00, 'Офисный эргономичный стул'),
+(4, 'Чайник', 'Бытовая техника', 45.50, 'Электрический чайник'),
+(5, 'Книга', 'Канцелярия', 18.75, 'Бестселлер художественной литературы');
+------------------------------------------------------------
+--task-21:
+SELECT * INTO Products_Backup
+FROM Products;
+------------------------------------------------------------
+--task-22:
+EXEC sp_rename 'Products', 'Inventory';
+------------------------------------------------------------
+--task-23:
+ALTER TABLE Inventory
+ALTER COLUMN Price FLOAT;
+------------------------------------------------------------
+--task-24:
+CREATE TABLE Inventory_New (
+    ProductCode INT IDENTITY(1000, 5) PRIMARY KEY,
+    ProductID INT,
+    ProductName VARCHAR(100),
+    ProductCategory VARCHAR(50),
+    Price FLOAT,
+    Description VARCHAR(255),
+    StockQuantity INT
+);
+--task-25:
+INSERT INTO Inventory_New (ProductID, ProductName, ProductCategory, Price, Description, StockQuantity)
+SELECT ProductID, ProductName, ProductCategory, Price, Description, StockQuantity
+FROM Inventory;
+DROP TABLE Inventory;
+EXEC sp_rename 'Inventory_New', 'Inventory';
